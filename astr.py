3e37dd86f696819eeebb55e535fc8379b4e79c3b
horseburger <pralka.gniewu@gmail.com>
2020-04-27 22:46:14 +0200

    fix: astr algo; refactor(astr): migrate from dicts to vars

--- astr.py
+++ astr.py
@@ -11,31 +11,36 @@ class ASTR(Strategy):
     def run(self):
         if self.model.is_solved(self.model.current_state):
             return []
-        path = []
-        best_state = {}
-        best_zero = {}
-        best_move = {}
+
+        best_state = self.model.current_state
+        best_zero = self.model.zero_position
+        best_move = None
+
         while True:
             self.explored.append(np.copy(self.model.current_state))
             self.explored_hash[sha256(self.model.current_state).hexdigest()] = True
+
             best_f_value = 120
+
             ops = self.model.get_operators()
+
             for op in ops:
                 new_state, new_zero = self.model.get_neighbour_state(op)
-                f_value = self.model.get_f_value(new_state, self.current_depth)
                 new_state_hash = sha256(new_state).hexdigest()
+
+                f_value = self.model.get_f_value(new_state, self.current_depth)
+
                 if f_value == 0:
                     self.path[0].append(op)
                     return self.path[0]
+
                 elif f_value <= best_f_value and not self.explored_hash[new_state_hash]:
-                    best_state[f_value] = new_state
-                    best_zero[f_value] = new_zero
-                    best_move[f_value] = op
-            key = list(best_state.keys())[0]
-            self.path[0].append(best_move[key])
-            self.model.current_state = np.copy(best_state[key])
-            self.model.zero_position = best_zero[key]
-            best_state.clear()
-            best_move.clear()
-            best_zero.clear()
+                    best_f_value = f_value
+                    best_state = new_state
+                    best_zero = new_zero
+                    best_move = op
+
+            self.path[0].append(best_move)
+            self.model.current_state = np.copy(best_state)
+            self.model.zero_position = best_zero
             self.current_depth += 1
